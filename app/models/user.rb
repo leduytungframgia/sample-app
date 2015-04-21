854ea3d056744c40adfa12c35370255576861cad
Kyle <tung.leduy.2207@gmail.com>
2015-04-21 01:38:15 +0000

    finish logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ActiveRecord::Base
     validates :email, presence:true, length: {maximum: 50},
                       format: {with: VALID_EMAIL_REGEX},
                       uniqueness: {case_sensitive: false}
+                      
     has_secure_password
+    
     validates :password, presence:true, length: {minimum: 6}
     
     def User.digest(string)
