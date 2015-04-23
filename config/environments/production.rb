8d3ffb810da7e06dda42c453090647cf3e42bc8d
Kyle <tung.leduy.2207@gmail.com>
2015-04-23 09:05:50 +0000

    active email 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'rails-tutorial-kylecrazy.c9.io'
+  host = 'sample-app-tung.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
